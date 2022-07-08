3669935ea64f65d6b35626d32f491f8c99fd9c85
Qark-dev <qarkdev@gmail.com>
2022-07-08 19:45:01 +0800

    smaller length to scroll

--- .config/polybar/scripts/mpc_zscroll.sh
+++ .config/polybar/scripts/mpc_zscroll.sh
@@ -4,6 +4,7 @@ zscroll --before-text "♪ x" --delay 0.2 \
 		--match-command "mpc status" \
 		--match-text "playing" "--before-text ' '" \
 		--match-text "paused" "--before-text ' ' --scroll 0" \
+		--length 14 \
 		--update-check true "mpc current" &
 
 wait
